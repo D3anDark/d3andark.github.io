1e523c7ca4fb1c8b2c7eb9645a0a4e868929a15e
DeanDark <min3st8@gmail.com>
2024-05-09 23:43:35 +0200

    make js mobile menu button
    Took 1 hour 1 minute

---
+++ src/js/dropdown.js
@@ -0,0 +1,10 @@
+function toggleDropdown() {
+    const dropdown = document.querySelector('.dropdown-content');
+    const dropdownButton = document.querySelector('.dropdown-button');
+
+
+    dropdown.classList.toggle('visible');
+    dropdownButton.classList.toggle('active');
+}
+
+
